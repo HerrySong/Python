003daac3ef87e28c5b8ebbf884c9195b651db8aa
RaphaelZheng <522475851@qq.com>
2017-05-26 16:54:04 +0800

    DetectThreshold Finish

--- 2-Mode Method.py
+++ 2-Mode Method.py
@@ -6,8 +6,8 @@ from matplotlib import pyplot as plt
 image = plt.imread("lena.jpg")
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-plt.subplot(221), plt.imshow(image, "gray")
-plt.title("source image"), plt.xticks([]), plt.yticks([])
+plt.subplot(221), plt.imshow(gray, "gray")
+plt.title("gray image"), plt.xticks([]), plt.yticks([])
 
 plt.subplot(222), plt.hist(image.ravel(), 256)
 plt.title("Histogram")
